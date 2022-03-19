ced7f02c8862342bb704fc9be3e1a19e32b4c3a8
ChoiMoonSeok <80397444+ChoiMoonSeok@users.noreply.github.com>
2022-03-19 18:59:05 +0900

    server start fix

--- server/__init__.py
+++ server/__init__.py
@@ -1,15 +1,8 @@
 from flask import Flask
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 import config
 
-from api import *
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
     
-    @app.route('/')
-    def hello():
-        return 'hello'
-
-    return app
+@app.route('/')
+def hello():
+    return 'hello'
